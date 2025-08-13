214483d73d2a495897da8cf6de7ea44eb3656f75
Sergey Getman <sergeygetman2020@gmail.com>
2025-08-14 01:17:59 +0300

    add project fireWall

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import './Index.style';
+import { HeaderAppStyle } from './Index.style';
+import AddPage from './components/AddPage';
+import AdditionalInfo from './components/AdditionalInfo';
+import { CustomButtonTextEnum } from './enam';
+import { useDispatch, useSelector } from 'react-redux';
+import { AccountPagesState } from './types';
+import { ButtonElement } from './components/button/ButtonElement';
+import { useNavigate } from 'react-router-dom';
+import { addSteper } from './store/globalSlice';
+import { useCallback } from 'react';
+import SearchField from './components/SearcField';
+
+export const App = () => {
+  const stepper = useSelector((state: AccountPagesState | any) => state.globalState.count);
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const handleCall = useCallback(() => {
+    dispatch(addSteper(stepper + 1));
+    navigate('/add');
+  }, [stepper]);
+
+  return (
+    <>
+      <HeaderAppStyle step={stepper}>
+        <SearchField />
+        <AddPage />
+        <AdditionalInfo />
+        <ButtonElement text={CustomButtonTextEnum.titleNext} handleClick={handleCall} variant="contained" />
+      </HeaderAppStyle>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/AddPage.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ButtonElement from './button/ButtonElement';
+import { getTransports } from '../api/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { addDataPages, getStatus } from '../store/globalSlice';
+import { AccountPagesState } from '../types';
+import { CustomTextEnum } from '../enam';
+
+const AddPage = () => {
+  const notify = () => toast(CustomTextEnum.textMessageAdd);
+  const [showNotif, setShowNotif] = useState(false);
+  const { status } = useSelector((state: AccountPagesState | any) => state.globalState);
+  const st = useSelector((state) => console.log('this is state222', state));
+  console.log(st);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getTransports().then((response) => {
+      const statusResponce = response.status;
+      dispatch(getStatus(status, response.status));
+      dispatch(addDataPages(response.data, response));
+      if (statusResponce == 200) {
+        setShowNotif(true);
+      }
+    });
+  }, []);
+  return (
+    <>
+      <ButtonElement onClick={notify}>{CustomTextEnum.Notification}</ButtonElement>
+      {showNotif && <ToastContainer />}
+    </>
+  );
+};
+
+export default AddPage;
